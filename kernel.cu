fde0ad91b51f30de7768341dffea7b11ccc8b4cc
Ilia Kravchuk <kilubaka@gmail.com>
2021-03-06 19:11:13 +0200

    Step1
    Basic camera and matrix setup.
    Colorful view of camera representing direction of vector. Right mouse click and drag will rotate camera

--- kernel.cu
+++ kernel.cu
@@ -1,22 +1,99 @@
 ﻿#include "device_launch_parameters.h"
+#include <helper_math.h> 
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <cuda_runtime.h>
 #include <cuda_gl_interop.h>
+#include <iostream>
 
 #define width 1024   //screen width
 #define height 1024   //screen height
 
+float t = 0.0f;   //timer
 float3* device;   //pointer to memory on the device (GPU VRAM)
 GLuint buffer;   //buffer
 
+__global__ void rendering(float3* output, float k, float rotX, float rotY)
+{
+	unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+	unsigned int i = (height - y - 1) * width + x;
+	float2 resolution = make_float2((float)width, (float)height);   //screen resolution
+	float2 coordinates = make_float2((float)x, (float)y);   //fragment coordinates
+
+	float2 uv = (2.0 * coordinates - resolution) / resolution.y;
+	float3 ro = make_float3(0.0f, 0.0f, -8.0f);   //ray origin
+
+	float finalX = uv.x + rotX;
+	float finalY = uv.y + rotY;
+
+	float3 rd = normalize(make_float3(finalX, finalY, 1.0f));   //ray direction
+
+	float3 c = rd;
+	float colour;
+	unsigned char bytes[] = { (unsigned char)(c.x * 255), (unsigned char)(c.y * 255), (unsigned char)(c.z * 255), 1 };
+
+	memcpy(&colour, &bytes, sizeof(colour));   //convert from 4 bytes to single float
+	output[i] = make_float3(x, y, colour);
+}
+
+void time(int x)
+{
+	if (glutGetWindow())
+	{
+		glutPostRedisplay();
+		glutTimerFunc(10, time, 0);
+		t += 0.01f;
+	}
+}
+
+
+
+// actual vector representing the camera's direction
+float rotX = 0.0f, rotY = 0.0f;
+float mouseSensivityX = 0.01f, mouseSensivityY = 0.003f;
+
+int mouseState = 0;
+
+void mouseButton(int button, int state, int x, int y) {
+
+	// only start motion if the left button is pressed
+	if (button == GLUT_LEFT_BUTTON) {
+
+		// when the button is pressed
+		mouseState = state == GLUT_DOWN ? 1 : 0;
+	}
+
+	if (button == GLUT_RIGHT_BUTTON) {
+		mouseState = state == GLUT_DOWN ? -1 : 0;
+	}
+
+}
+
+void mouseMove(int x, int y) {
+	glutWarpPointer(width / 2, height / 2);
+
+	// this will only be true when the right button is down
+	if (mouseState == -1) {
+		// update camera's direction
+		rotX += (x - width / 2) * mouseSensivityX;
+		rotY += -(y - height / 2) * mouseSensivityY;
+
+		if (fabs(rotX) > 180) rotX = clamp(-rotX, -180.0f, 180.0f);
+		if (fabs(rotY) > 180) rotY = clamp(-rotY, -180.0f, 180.0f);
+
+		std::cout << "rotX: " << rotX << "\trotY: " << rotY << std::endl;
+	}
+}
 
 void display(void)
 {
 	cudaThreadSynchronize();
 	cudaGLMapBufferObject((void**)&device, buffer);   //maps the buffer object into the address space of CUDA
 	glClear(GL_COLOR_BUFFER_BIT);
-
+	dim3 block(16, 16, 1);
+	dim3 grid(width / block.x, height / block.y, 1);
+	rendering <<< grid, block >>> (device, t, rotX, rotY);   //execute kernel
 	cudaThreadSynchronize();
 	cudaGLUnmapBufferObject(buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
@@ -41,7 +118,10 @@ int main(int argc, char** argv)
 	glMatrixMode(GL_PROJECTION);
 	gluOrtho2D(0.0, width, 0.0, height);
 	glutDisplayFunc(display);
+	glutMouseFunc(mouseButton);
+	glutMotionFunc(mouseMove);
 
+	time(0);
 	glewInit();
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
